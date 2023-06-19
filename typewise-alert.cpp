3b2637fcb4c4b546f9e0560bf46822b6ce4abeb1
Kanojia Raghav Krishnakant <raghavkrishnakant.kanojia@bosch.com>
2023-06-19 12:11:53 +0530

    Itration 1

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,35 +1,35 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
-  if(value < lowerLimit) {
-    return TOO_LOW;
+BreachType inferBreach(double value, double upperLimit) {
+  BreachType returnVal = NORMAL;
+  if(value < 0) {
+      returnVal = TOO_LOW;
   }
-  if(value > upperLimit) {
-    return TOO_HIGH;
+  else if(value > upperLimit) {
+      returnVal = TOO_HIGH;
   }
-  return NORMAL;
+  return returnVal;
 }
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-  int lowerLimit = 0;
   int upperLimit = 0;
   switch(coolingType) {
     case PASSIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 35;
       break;
     case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 45;
       break;
     case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
       upperLimit = 40;
       break;
+    default:
+      upperLimit = 35;
+      break;
   }
-  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+  return inferBreach(temperatureInC, upperLimit);
 }
 
 void checkAndAlert(
