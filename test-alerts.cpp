cb9dbb4e8df95cd507c18883d436a890e08cd721
Kanojia Raghav Krishnakant <raghavkrishnakant.kanojia@bosch.com>
2023-06-22 12:18:34 +0530

    adding reflection try 1

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,23 @@
 #include "catch.hpp"
 #include "typewise-alert.h"
 
+template <typename E, E V> constexpr bool IsValid() {
+	std::string name = __FUNCSIG__;
+	printf("%s\n", name.c_str());
+	int i = strlen(name.c_str());
+	for (; i >= 0; --i) {
+		if (name[i] == ' ') {
+			break;
+		}
+	}
+	char c = name[i + 1];
+	printf("%c, \n", c);
+	if (c >= '0' && c <= '3') {
+		return false;
+	}
+	return true;
+}
+
 TEST_CASE("infers the breach according to Low Limit") {
 	REQUIRE(inferBreach(-1, 30) == TOO_LOW);
 }
@@ -20,7 +37,21 @@ TEST_CASE("infers the breach boundry check") {
 }
 
 TEST_CASE("classifyTemperatureBreach CoolingType is PASSIVE_COOLING and temp is normal") {
-	REQUIRE(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 25) == NORMAL);
+	bool x = IsValid<CoolingType, PASSIVE_COOLING>();
+	if (x)
+	{
+		printf("This is correct");
+		REQUIRE(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 25) == NORMAL);
+	}
+}
+
+TEST_CASE("classifyTemperatureBreach CoolingType is PASSIVE_COOLING and temp is normal invalid") {
+	bool x = IsValid<CoolingType, (CoolingType)4>();
+	if (x)
+	{
+		printf("This is correct");
+		REQUIRE(classifyTemperatureBreach(CoolingType::PASSIVE_COOLING, 25) == NORMAL);
+	}
 }
 
 TEST_CASE("classifyTemperatureBreach CoolingType is HI_ACTIVE_COOLING and temp is normal") {
